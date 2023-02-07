31455022e1bf1ddffe4f9e061da21b9243da8f3c
AgustinJaime99 <agustindiegojaime@gmail.com>
2023-02-07 18:57:07 -0300

    Update: animations and image in card product

--- Client/components/common/ProductCard/index.js
+++ Client/components/common/ProductCard/index.js
@@ -1,12 +1,24 @@
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
+import ImageMock from '@/public/images/img_keyboard_test.png'
+import Image from 'next/image'
 
 export const ProductCard = ({ img, title, details, price }) => {
-  return <CardContainer>index</CardContainer>
+  return (
+    <CardContainer whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.99 }}>
+      <div className="top-info">
+        <Image src={ImageMock} />
+        <span className="divider" />
+        <h3>{title}</h3>
+      </div>
+    </CardContainer>
+  )
 }
 
-const CardContainer = styled.article`
+const CardContainer = styled(motion.article)`
   height: 53rem;
   width: 33rem;
   box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.1);
   margin: 3rem auto;
+  background-color: var(--gray4);
 `
