da96e32ae4477f09d1e77b2f63900204ad18a280
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-11 14:08:02 -0300

    Update: Add UserRepository Interface in the Repository package

--- API/src/main/java/com/spartanHardware/repository/UserRepository.java
+++ API/src/main/java/com/spartanHardware/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.spartanHardware.repository;
 
-public class UserRepository {
+import com.spartanHardware.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
