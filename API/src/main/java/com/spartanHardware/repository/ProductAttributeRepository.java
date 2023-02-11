88a2748e6ed996ea3dc301a97462bb3647346843
CarolinaCamilaQuevedo <CarolinaCQ>
2023-02-11 18:28:01 -0300

    Feat: add Product Attribute entity and repository

---
+++ API/src/main/java/com/spartanHardware/repository/ProductAttributeRepository.java
@@ -0,0 +1,9 @@
+package com.spartanHardware.repository;
+
+import com.spartanHardware.model.entity.ProductAttribute;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductAttributeRepository extends JpaRepository<ProductAttribute, Long> {
+}
