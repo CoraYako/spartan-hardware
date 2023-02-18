1c6a81e7baf7d37b15b717e06bfc9be185afdbb3
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-18 08:42:03 -0300

    Update: transform AddressRequestDTO and AddressResponseDTO into classes and add the variables

--- API/src/main/java/com/spartanHardware/model/dto/request/AddressRequestDTO.java
+++ API/src/main/java/com/spartanHardware/model/dto/request/AddressRequestDTO.java
@@ -1,11 +1,18 @@
 package com.spartanHardware.model.dto.request;
 
-public record AddressRequestDTO(
-        String street,
-        String number,
-        String departamentNumber,
-        String city,
-        String province,
-        Boolean defaultAddress
-) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@AllArgsConstructor
+@Data
+public class AddressRequestDTO{
+    private String street;
+    private String number;
+    private String departamentNumber;
+    private String departamentFloor;
+    private String postalCode;
+    private String city;
+    private String province;
+    private Boolean defaultAddress;
 }
--- API/src/main/java/com/spartanHardware/model/dto/response/AddressResponseDTO.java
+++ API/src/main/java/com/spartanHardware/model/dto/response/AddressResponseDTO.java
@@ -1,11 +1,18 @@
 package com.spartanHardware.model.dto.response;
 
-public record AddressResponseDTO(
-        String street,
-        String number,
-        String departamentNumber,
-        String city,
-        String province,
-        Boolean defaultAddress
-) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class AddressResponseDTO{
+    private Long id;
+    private String street;
+    private String number;
+    private String departamentNumber;
+    private String departamentFloor;
+    private String postalCode;
+    private String city;
+    private String province;
+    private Boolean defaultAddress;
 }
