92302cf7fcb13eb5a1a02f226b3005c9eb56528e
CarolinaCamilaQuevedo <CarolinaCQ>
2023-02-20 00:23:50 -0300

    Update: change records to class

--- API/src/main/java/com/spartanHardware/model/dto/request/UserRequestDTO.java
+++ API/src/main/java/com/spartanHardware/model/dto/request/UserRequestDTO.java
@@ -1,9 +1,13 @@
 package com.spartanHardware.model.dto.request;
 
-public record UserRequestDTO(
-        String email,
-        String password,
-        String firstName,
-        String lastName
-) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserRequestDTO {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
 }
--- API/src/main/java/com/spartanHardware/model/dto/response/UserResponseDTO.java
+++ API/src/main/java/com/spartanHardware/model/dto/response/UserResponseDTO.java
@@ -1,15 +1,18 @@
 package com.spartanHardware.model.dto.response;
 
 import com.spartanHardware.model.entity.Authority;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record UserResponseDTO(
-        String email,
-        String firstName,
-        String lastName,
-        List<String> authorities,
-        LocalDateTime creationDate
-) {
+@Data
+@AllArgsConstructor
+public class UserResponseDTO {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private List<String> authorities;
+    private LocalDateTime creationDate;
 }
