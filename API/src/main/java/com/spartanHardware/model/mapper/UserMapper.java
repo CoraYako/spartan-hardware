968b4b437feee17c77f2e8fa263d158177f78282
CarolinaCamilaQuevedo <CarolinaCQ>
2023-02-20 00:24:51 -0300

    Update: add methods

--- API/src/main/java/com/spartanHardware/model/mapper/UserMapper.java
+++ API/src/main/java/com/spartanHardware/model/mapper/UserMapper.java
@@ -6,13 +6,15 @@ import com.spartanHardware.model.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Component
 public class UserMapper {
 
     public UserResponseDTO toDto(User user) {
-        UserResponseDTO dto = new  UserResponseDTO(
+        return new  UserResponseDTO(
                 user.getUsername(),
                 user.getFirstName(),
                 user.getLastName(),
@@ -22,16 +24,26 @@ public class UserMapper {
                         .collect(Collectors.toList()),
                 user.getCreationDate()
         );
-        return dto;
     }
 
     public User toUser(UserRequestDTO dto) {
         User user = new User();
-        user.setEmail(dto.email());
-        user.setFirstName(dto.firstName());
-        user.setLastName(dto.lastName());
-        user.setPassword(dto.password());
+        user.setEmail(dto.getEmail());
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
         user.setEnabled(true);
         return user;
+    }
+
+    public User toUpdatedUser(UserRequestDTO dto, User user) {
+        if(!Objects.isNull(dto.getEmail())) user.setEmail(dto.getEmail());
+        if(!Objects.isNull(dto.getPassword())) user.setPassword(dto.getPassword());
+        if(!Objects.isNull(dto.getFirstName())) user.setFirstName(dto.getFirstName());
+        if(!Objects.isNull(dto.getLastName())) user.setLastName(dto.getLastName());
+        return user;
+    }
+
+    public List<UserResponseDTO> toDtoList (List<User> users){
+        return users.stream().map(user -> toDto(user)).collect(Collectors.toList());
     }
 }
