a9288d2a217c6e2f3e68bfaf9d9e2b7c6f9c5f3d
CarolinaCamilaQuevedo <CarolinaCQ>
2023-02-20 00:25:56 -0300

    Update: refactor code

--- API/src/main/java/com/spartanHardware/service/IAuthorityService.java
+++ API/src/main/java/com/spartanHardware/service/IAuthorityService.java
@@ -1,7 +1,5 @@
 package com.spartanHardware.service;
 
-import com.spartanHardware.model.dto.request.AuthorityRequestDTO;
-import com.spartanHardware.model.dto.response.AuthorityResponseDTO;
 import com.spartanHardware.model.entity.Authority;
 
 import java.util.List;
