3098fc4d4e88c8f0856d44b23e5f86951dd89b7f
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-21 19:12:00 -0300

    Update: add update, get and delete methods to complete the address CRUD

--- API/src/main/java/com/spartanHardware/service/IAddressService.java
+++ API/src/main/java/com/spartanHardware/service/IAddressService.java
@@ -2,8 +2,16 @@ package com.spartanHardware.service;
 
 import com.spartanHardware.model.dto.request.AddressRequestDTO;
 import com.spartanHardware.model.dto.response.AddressResponseDTO;
+import com.spartanHardware.model.entity.Address;
+
+import java.util.List;
 
 public interface IAddressService {
 
     AddressResponseDTO registerAddress(AddressRequestDTO dto);
+    AddressResponseDTO updateAddress(AddressRequestDTO dto, Long id);
+    List<AddressResponseDTO> getAllAddresses();
+    Address getAddressById(Long id);
+    AddressResponseDTO getAddressDtoById(Long id);
+    void deleteAddressById(Long id);
 }
--- API/src/main/java/com/spartanHardware/service/impl/AddressServiceImpl.java
+++ API/src/main/java/com/spartanHardware/service/impl/AddressServiceImpl.java
@@ -3,11 +3,17 @@ package com.spartanHardware.service.impl;
 import com.spartanHardware.model.dto.request.AddressRequestDTO;
 import com.spartanHardware.model.dto.response.AddressResponseDTO;
 import com.spartanHardware.model.entity.Address;
+import com.spartanHardware.model.entity.User;
 import com.spartanHardware.model.mapper.AddressMapper;
 import com.spartanHardware.repository.AddressRepository;
 import com.spartanHardware.service.IAddressService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,10 +23,52 @@ public class AddressServiceImpl implements IAddressService {
     private final AddressMapper mapper;
 
     @Override
+    @Transactional
     public AddressResponseDTO registerAddress(AddressRequestDTO dto) {
         Address address = mapper.toAddress(dto);
         Address savedAddress = repository.save(address);
 
         return mapper.toDto(savedAddress);
+    }
+
+    @Override
+    @Transactional
+    public AddressResponseDTO updateAddress(AddressRequestDTO dto, Long id) {
+        Address address = getAddressById(id);
+        User user = new User();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(!auth.getName().equalsIgnoreCase(user.getUsername()))
+            System.out.println("Usuario no coincide");
+        Address updatedAddress = mapper.toUpdatedAddress(dto,address);
+
+        return mapper.toDto(repository.save(updatedAddress));
+    }
+
+    @Override
+    public List<AddressResponseDTO> getAllAddresses() {
+        return mapper.toDtoList(repository.findAll());
+    }
+
+    @Override
+    public Address getAddressById(Long id) {
+        if(!repository.findById(id).isPresent())
+            System.out.println("La dirección no coincide");
+        return repository.findById(id).get();
+    }
+
+    @Override
+    public AddressResponseDTO getAddressDtoById(Long id) {
+        return mapper.toDto(getAddressById(id));
+    }
+
+    @Override
+    @Transactional
+    public void deleteAddressById(Long id) {
+        Address address = getAddressById(id);
+        User user = new User();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(!auth.getName().equalsIgnoreCase(user.getUsername()))
+            System.out.println("Usuario no coincide");
+        repository.deleteById(id);
     }
 }
