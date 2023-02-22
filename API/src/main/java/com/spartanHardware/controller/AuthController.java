586006b5fee4cea74a439a87d2f34cea44d0cf95
CarolinaCamilaQuevedo <CarolinaCQ>
2023-02-22 15:36:13 -0300

    Update: change endpoint url

--- API/src/main/java/com/spartanHardware/controller/AuthController.java
+++ API/src/main/java/com/spartanHardware/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
