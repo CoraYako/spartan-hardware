f9ca0a31cf5f3563acede70018e937d2aec55563
CarolinaCamilaQuevedo <CarolinaCQ>
2023-02-22 12:03:12 -0300

    Feat: add jwt filter and service

--- API/src/main/java/com/spartanHardware/auth/security/AppSecurity.java
+++ API/src/main/java/com/spartanHardware/auth/security/AppSecurity.java
@@ -1,30 +1,25 @@
 package com.spartanHardware.auth.security;
 
+import com.spartanHardware.auth.filter.JWTAuthenticationFilter;
 import com.spartanHardware.model.enums.Role;
-import com.spartanHardware.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.*;
 
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class AppSecurity {
-
-    private final UserRepository repository;
+    private final JWTAuthenticationFilter jwtFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -33,42 +28,21 @@ public class AppSecurity {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/register-user")
+                .requestMatchers("/auth/register-user", "/auth/login")
                 .permitAll()
+                .requestMatchers(POST, "/auth/register-admin").hasAnyAuthority(Role.ADMIN.getSimpleRoleName())
+                .requestMatchers(GET, "/users").hasAnyAuthority(Role.ADMIN.getSimpleRoleName())
+                .requestMatchers(GET, "/users/{id}").hasAnyAuthority(Role.ADMIN.getSimpleRoleName())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authenticationProvider(authenticationProvider())
-        //.addFilterBefore()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
         ;
 
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return username -> repository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException(""));
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService());
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
