b4ce29bdc5896486042be367d48b6947e6ba1ff8
CarolinaCamilaQuevedo <CarolinaCQ>
2023-02-22 12:13:31 -0300

    Update: change user

--- API/src/main/java/com/spartanHardware/model/entity/User.java
+++ API/src/main/java/com/spartanHardware/model/entity/User.java
@@ -103,6 +103,9 @@ public class User implements UserDetails {
     }
 
     @Override
+    public String getPassword(){ return password;}
+
+    @Override
     public boolean isAccountNonExpired() {
         return true;
     }
