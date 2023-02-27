1e40f8d4435fa6b6ba3ed4b540d733cf6d4a1cf0
AgustinJaime99 <agustindiegojaime@gmail.com>
2023-02-27 16:15:22 -0300

    Fix: Resolve merge conflicts

--- Client/components/public/home/Recommned.js
+++ Client/components/public/home/Recommned.js
@@ -4,13 +4,15 @@ import RecommendCard from '../../common/Cardrecord'
 export const Recommend = () => {
   return (
     <>
-      <div className="title">
-        <h2>PC's Recomendadas</h2>
-      </div>
       <CardContainer>
-        <RecommendCard title={"PC's BÁSICA"} link="/pc-recommend-sale" />
-        <RecommendCard title={"PC's MEDIA"} link="/pc-recommend-sale" />
-        <RecommendCard title={"PC's ALTA"} link="/pc-recommend-sale" />
+        <div className="title">
+          <h2>PC's Recomendadas</h2>
+        </div>
+        <div className="recommend">
+          <RecommendCard title={"PC's BÁSICA"} link="/pc-recommend-sale" />
+          <RecommendCard title={"PC's MEDIA"} link="/pc-recommend-sale" />
+          <RecommendCard title={"PC's ALTA"} link="/pc-recommend-sale" />
+        </div>
       </CardContainer>
     </>
   )
@@ -24,5 +26,18 @@ const CardContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  overflow: hidden;
+
+  .title {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    gap: 22rem;
+  }
+
+  .recommend {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 12em;
+  }
 `
